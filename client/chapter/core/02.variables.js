fba3432ab097f9de4f3129f6eb50729d53bab7b0
ttining-real <ttining.real@gmail.com>
2024-05-30 08:28:13 +0900

    [24.05.29] JS 코어 - 코드 구조 - 변수 - 엄격 모드 - 전역 객체 - 오래된 var

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,48 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin;
+let nickName;
+
+nickName = 'John';
+admin = nickName;
+
+console.log(admin);
+
+
+let OUR_PLANET_NAME = 'Earth';
+let currentOnlineUserName = 'ttining';
+
+console.log(OUR_PLANET_NAME);
+console.log(currentOnlineUserName);
+
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기 'calcProductPriceQuantity'
+// - 구매 제품 가격의 총 합 'totalProductPrice'
+// - 1년 기준 일(day)자 수 : 365일 'DAY_PER_YEAR'
+// - 구매 결제 여부 
+// true/false => boolean data를 값으로 갖는 경우 is/has네이밍을 사용하는 것이 관례이다.
+// let isPayment = false;
+// let hasCartList = false;
+
+// - 구매 결제 내역
+// let paymentList;
+// - 브랜드 접두사
+// - 오늘의 운세
+// - 상품 정보
+
+/* variables ----------------------------------------------------------- */
+let calcProductPriceQuantity;
+let totalProductPrice;
+let isPayment;
+let hasCartList;
+let fortuneToday = 'so good';
+
+/* constant variables -------------------------------------------------- */
+const DAY_PER_YEAR = 365;
+const BRAND_PREFIX = 'TheNorthFace';
+const productInfo = '';
--- client/main.js
+++ client/main.js
@@ -1,3 +1,8 @@
 const a = 'hello';
 
 console.log('안녕 웹 브라우저!');
+
+
+// alert('그만.');
+// confirm('정말 지울거야...?');
+prompt('당신의 이름은 무엇입니까?');

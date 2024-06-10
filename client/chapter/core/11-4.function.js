ff58bf57560653e861715fed1d6c1dcfc2255820
ttining-real <ttining.real@gmail.com>
2024-06-10 16:03:28 +0900

    [24.06.07] JS 코어 * function (함수) - 화살표 함수 - 재귀 함수

--- client/chapter/core/11-4.function.js
+++ client/chapter/core/11-4.function.js
@@ -99,34 +99,18 @@ function fibonacci(n) {
 
 // const cache = {}
 
-// const memofibo = (n) => {
-//   if ( n <= 0 ) return 0;
-//   if ( n <= 2 ) return 1;
-
-//   
-//   if(memoFibo.cache[n]) {
-//     return memoFibo.cache[n];
-//   }
-//   
-//   else {
-//     return memoFibo.cache[n] = memofibo(n-1) + memofibo(n-2);
-//   }
-// }
-
-// memoFibo.cache = {}
 const memoFibo = (n)=>{
   if(n <= 0) return 0;
   if(n <= 2) return 1;
 
-  // 이전 값이 있다면 그걸 그대로 사용하고
   if(memoFibo.cache[n]){
     return memoFibo.cache[n];
   }
-  // 없다면 계산하도록 함
   else{
     return memoFibo.cache[n] = memoFibo(n-1) + memoFibo(n-2);
   }
 }
+
 
 memoFibo.cache = {}
 
@@ -176,20 +160,6 @@ const SocialPartiners = {
   },
 };
 
-// function sumSalaries(department) {
-
-//   if(Array.isArray(department)) {
-//     department.reduce((acc, cur) => acc + cur.salary, 0)
-//   } else {
-//     let sum = 0;
-//     for ( let sub of Object.values(department)) {
-//       sum += sumSalaries(sub)
-//     }
-
-//     return sum;
-//   }
-// }
-
 function sumSalaries(department){
   
   if(Array.isArray(department)){
@@ -200,22 +170,74 @@ function sumSalaries(department){
     for(let sub of Object.values(department)){
       sum += sumSalaries(sub)
     }
-  
     return sum;
-    
   }
 }
 
 
-function print(data) {
+function isArray(data){
+  return Array.isArray(data)
+}
 
+
+const randomUser = {
+  gender: 'female',
+  name: { title: 'Ms', first: 'Carol', last: 'May' },
+  location: {
+    street: { number: 9162, name: 'Church Road' },
+    city: 'Birmingham',
+    state: 'Cumbria',
+    country: 'United Kingdom',
+    postcode: 'FO5E 4TN',
+    coordinates: { latitude: '-4.3301', longitude: '155.0223' },
+    timezone: { offset: '-4:00', description: 'Atlantic Time (Canada), Caracas, La Paz' },
+  },
+  email: 'carol.may@example.com',
+  login: {
+    uuid: '39e4e214-7f66-44a6-a3ba-3b5ce46b8e25',
+    username: 'redduck745',
+    password: 'picks',
+    salt: '8xzqOzAn',
+    md5: '7250e4042c2367cc82487f798c7c5253',
+    sha1: '6c0e2fac669d6d7f11fb0bab52493f441cf5834b',
+    sha256: '9e49256b8917113750533c24c015336af43d5d7130cf8faa19054c1ba36e7de8',
+  },
+  dob: { date: '1962-12-07T21:51:26.781Z', age: 59 },
+  registered: { date: '2018-06-08T04:07:17.788Z', age: 4 },
+  phone: '022 1280 9236',
+  cell: '07653 428700',
+  id: { name: 'NINO', value: 'SH 44 98 72 L' },
+  picture: {
+    large: 'https://randomuser.me/api/portraits/women/21.jpg',
+    medium: 'https://randomuser.me/api/portraits/med/women/21.jpg',
+    thumbnail: 'https://randomuser.me/api/portraits/thumb/women/21.jpg',
+  },
+  nat: 'GB',
+};
+
+
+function print(data){
+
+  if(Object.prototype.toString.call(data).slice(8,-1).toLowerCase() === 'object'){
+    for(let keyValue of Object.entries(data)){
+      let key = keyValue[0]
+      let value = keyValue[1]
   
-  // Object.entries
-
-  for(let keyValue of Object.entries(data)) {
-    let key = key
-    let value = 
+      if(typeof value === 'object' || isArray(value)){
+        print(value)
+      }else{
+        console.log(key,' : ' ,value);
+      }
+    }
   }
 
-
+  if(isArray(data)){
+    data.forEach((value,index)=>{
+      if(typeof value === 'object' || isArray(value)){
+        print(value)
+      }else{
+        console.log(index,' : ' ,value);
+      }
+    })
+  }
 }

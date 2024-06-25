472874b10f6416e026bc13ceaeb9759bd5a2a4a1
ttining-real <ttining.real@gmail.com>
2024-06-25 11:38:52 +0900

    [24.06.25] learn. random dice - renderItem - createItem - endScroll - reset

--- client/lib/dom/index.js
+++ client/lib/dom/index.js
@@ -10,5 +10,6 @@ export * from './insert.js';
 export * from './getNode.js';
 export * from './showAlert.js';
 export * from './bindEvent.js';
+export * from './endScroll.js';
 export * from './clearContents.js';
 export {default as clearContents} from './clearContents.js';

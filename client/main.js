e8e9dda429302982daa9690e3f57fa1f7f66002b
ttining-real <ttining.real@gmail.com>
2024-07-03 10:01:30 +0900

    [24.07.03] learn. web component - createElement - connectedCallback - disconnectedCallback

--- client/main.js
+++ client/main.js
@@ -1,40 +1,32 @@
-import { 
-  getNode,
-  getStorage,
-  setStorage,
-  deleteStorage,
-  clearContents,
- } from "./lib/index.js";
+
+// * 2
+// HTMLElement을 확장시켜 MyElement를 만들겠다. (MyElement라는 커스텀 태그를 만듦)
+class MyElement extends HTMLElement {
+  
+  constructor() {
+    super(); // 부모의 능력을 내가 상속 받겠다. (super를 써주지 않으면 에러가 난다.)
+  }
+
+  // 엘리먼트가 문서에 추가될 때 브라우저가 호출하는 메서드
+  connectedCallback() {
+    console.log('탄생함');
+  }
+
+  // 해제됐을 때 호출되는 메서드
+  disconnectedCallback() {
+    console.log('죽음');
+  }
+}
+
+// 태그 이름 정하기  (사용할 태그 이름, 컴포넌트 연결)
+customElements.define('c-element', MyElement)
 
 
 
-const textField = getNode('#textField');
-const clear = getNode('button[data-name="clear"]');
+// * 2
+// document.body.appendChild를 이용한 태그 생성도 가능하다. (이걸 더 많이 사용)
+const elem = document.createElement('c-element');
+// document.body.appendChild(elem);
 
-getStorage('text')
-.then((res) => {
-  textField.value = res;
-})
-
-
-
- function handleTextField(){
-  console.log('입력 중..');
-
-  const value = this.value;
-  // console.log(value);
-
-  setStorage('text', value);
- }
-
-
- 
- function handleClear() {
-  deleteStorage('text');
-  clearContents(textField);
- }
-
-
-
- textField.addEventListener('input',handleTextField);
- clear.addEventListener('click',handleClear);
+const app = document.getElementById('app')
+app.appendChild(elem);

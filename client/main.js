346e2d931cc692a61391a5b75c8b7318866bbd8f
ttining-real <ttining.real@gmail.com>
2024-07-03 10:28:14 +0900

    [24.07.03] learn. webComponent - observedAttributes - attributeChangedCallback

--- client/main.js
+++ client/main.js
@@ -1,32 +1,48 @@
 
-// * 2
-// HTMLElement을 확장시켜 MyElement를 만들겠다. (MyElement라는 커스텀 태그를 만듦)
-class MyElement extends HTMLElement {
-  
+class Button extends HTMLElement {
+
   constructor() {
-    super(); // 부모의 능력을 내가 상속 받겠다. (super를 써주지 않으면 에러가 난다.)
+    super();
+
+    this.button = document.querySelector('button');
+    
   }
 
-  // 엘리먼트가 문서에 추가될 때 브라우저가 호출하는 메서드
   connectedCallback() {
-    console.log('탄생함');
+    this._render(); // 연결이 될 때 1회 호출
   }
 
-  // 해제됐을 때 호출되는 메서드
   disconnectedCallback() {
-    console.log('죽음');
+
+  }
+
+  static get observedAttributes() {
+    // id 값을 감시
+    return ['id'];
+  }
+
+  // 속성이 변경될 때마다 같이 변경된다.
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log( name, oldValue, newValue );
+    
+    // 속성이 변경되면 _render 다시 호출
+    if(oldValue !== newValue) {
+      this._render();
+    }
+  }
+
+  // _render 메서드
+  _render() {
+    this.button.textContent = this.id;
   }
 }
 
-// 태그 이름 정하기  (사용할 태그 이름, 컴포넌트 연결)
-customElements.define('c-element', MyElement)
+// 'c-button' 버튼 정의, Button 클래스와 연결
+customElements.define('c-button', Button);
 
+const c = document.querySelector('c-button');
+let count = 0;
 
-
-// * 2
-// document.body.appendChild를 이용한 태그 생성도 가능하다. (이걸 더 많이 사용)
-const elem = document.createElement('c-element');
-// document.body.appendChild(elem);
-
-const app = document.getElementById('app')
-app.appendChild(elem);
+c.addEventListener('click', () => {
+  c.setAttribute('id', ++count);
+})

458ba2f9ae5142f91f9c75dac2af07c3b0dddd8f
ttining-real <ttining.real@gmail.com>
2024-07-03 13:45:52 +0900

    [24.07.03] learn. webComponent - event binding

--- client/main.js
+++ client/main.js
@@ -7,25 +7,42 @@ class UserCard extends HTMLElement {
     // shadow dom
     this.attachShadow({mode: 'open'});
     this.shadowRoot.innerHTML = `
-      <style>
-        :host{
-          background-color: orange;
-        }
-        div{
-          background:red;
-        }
-      </style>
-      <div> nickName : ttining-real </div>
-        <slot name="username"></slot>
-        <slot name="age"></slot>
-        <slot name="gender"></slot>
-        <slot name="common"></slot>
-        <slot></slot>
-        <slot name="email">any@naver.com</slot>
+      <button type="button">btn</button>
     `;
+
+    this.button = this.shadowRoot.querySelector('button')
+    // console.log(this.button);
+  }
+
+  connectedCallback() {
+    // click 했을 때 this가 clickMe 메서드를 실행
+    // this.button.addEventListener('click', this.clickMe) // user-card가 아니라 button이 나옴
+    this.button.addEventListener('click', () => this.clickMe()) // arrow function으로 상위 스코프의 this 가져다 씀
+    // this.button.addEventListener('click', this.clickMe.bind(this)) // bind로 this 묶어줌
+  }
+
+  // 메서드 정의
+  clickMe() {
+    console.log('thank you 🤗');
+    console.log(this);
   }
 
 }
 
 
 customElements.define('user-card', UserCard);
+
+// console.log( document.querySelector('user-card').shadowRoot.querySelector('button'))
+
+
+// function sum(){
+//   console.log(this);
+// }
+
+
+// sum() // undefined
+// sum.call({}) // {} => 실행
+// sum.apply({}) // {} => 실행
+// const n = sum.bind({}) // {} => 실행 x 
+
+// n()
